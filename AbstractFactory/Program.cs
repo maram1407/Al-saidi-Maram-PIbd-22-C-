378aefcdce2a61201b3a3772dcf535b1afb2a59b
aimanydammag <aimanydammag@outlook.com>
2020-08-29 23:31:01 -0800

    4

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -2,9 +2,9 @@
 using AbstractFactoryBusinessLogic.Interfaces;
 using AbstractFactoryDatabaseImplement.Implements;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.Windows.Forms;
 using Unity;
 using Unity.Lifetime;
@@ -42,6 +42,6 @@ namespace AbstractFactory
             }
         }
     }
- }
+}
 
     
